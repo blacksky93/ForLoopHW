e7401fdce3a41a38c43b34a8bb518efde2e08489
mgapp <mgapp@MIKEGYO>
2025-06-03 18:17:34 -0400

    Add project files.

---
+++ ForLoopHW/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForLoopHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter comma-separated list of first names: ");
+            string stringCsv = Console.ReadLine();
+            List<string> stringList = new List<string>(stringCsv.Split(','));
+            for (int firstNamePosition = 0; firstNamePosition < stringList.Count; firstNamePosition++)
+            {
+                switch (stringList[firstNamePosition])
+                {
+                    case "tim":
+                        Console.WriteLine($"Hello Professor {stringList[firstNamePosition]}");
+                        break;
+                    default:
+                        Console.WriteLine($"Hello {stringList[firstNamePosition]}");
+                        break;
+                }
+            }
+        }
+    }
+}
